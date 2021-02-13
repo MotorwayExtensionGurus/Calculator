c5129503ea33c9ff2dae1107769b59b2861baf01
Jonathan <70881466+Jonanen@users.noreply.github.com>
2021-02-13 20:50:21 +0100

    Add files via upload

---
+++ MEGClac/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace MEGClac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPlayer1 = new System.Windows.Forms.TextBox();
+            this.tbPlayer2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProcentName = new System.Windows.Forms.Label();
+            this.Netherrack = new System.Windows.Forms.Label();
+            this.Distance = new System.Windows.Forms.Label();
+            this.Procent = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbPasta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbPlayer1
+            // 
+            this.tbPlayer1.Location = new System.Drawing.Point(27, 36);
+            this.tbPlayer1.Name = "tbPlayer1";
+            this.tbPlayer1.Size = new System.Drawing.Size(202, 20);
+            this.tbPlayer1.TabIndex = 0;
+            this.tbPlayer1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbPlayer2
+            // 
+            this.tbPlayer2.Location = new System.Drawing.Point(27, 73);
+            this.tbPlayer2.Name = "tbPlayer2";
+            this.tbPlayer2.Size = new System.Drawing.Size(202, 20);
+            this.tbPlayer2.TabIndex = 1;
+            this.tbPlayer2.TextChanged += new System.EventHandler(this.tbPlayer2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Netherrack mined";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Distance";
+            // 
+            // ProcentName
+            // 
+            this.ProcentName.AutoSize = true;
+            this.ProcentName.Location = new System.Drawing.Point(100, 162);
+            this.ProcentName.Name = "ProcentName";
+            this.ProcentName.Size = new System.Drawing.Size(72, 13);
+            this.ProcentName.TabIndex = 4;
+            this.ProcentName.Text = "ProcentName";
+            // 
+            // Netherrack
+            // 
+            this.Netherrack.AutoSize = true;
+            this.Netherrack.Location = new System.Drawing.Point(24, 132);
+            this.Netherrack.Name = "Netherrack";
+            this.Netherrack.Size = new System.Drawing.Size(35, 13);
+            this.Netherrack.TabIndex = 5;
+            this.Netherrack.Text = "label3";
+            // 
+            // Distance
+            // 
+            this.Distance.AutoSize = true;
+            this.Distance.Location = new System.Drawing.Point(181, 132);
+            this.Distance.Name = "Distance";
+            this.Distance.Size = new System.Drawing.Size(35, 13);
+            this.Distance.TabIndex = 6;
+            this.Distance.Text = "label4";
+            // 
+            // Procent
+            // 
+            this.Procent.AutoSize = true;
+            this.Procent.Location = new System.Drawing.Point(119, 175);
+            this.Procent.Name = "Procent";
+            this.Procent.Size = new System.Drawing.Size(35, 13);
+            this.Procent.TabIndex = 7;
+            this.Procent.Text = "label5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "N";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(235, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "NE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbPasta
+            // 
+            this.tbPasta.Location = new System.Drawing.Point(12, 203);
+            this.tbPasta.Multiline = true;
+            this.tbPasta.Name = "tbPasta";
+            this.tbPasta.Size = new System.Drawing.Size(245, 170);
+            this.tbPasta.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 385);
+            this.Controls.Add(this.tbPasta);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Procent);
+            this.Controls.Add(this.Distance);
+            this.Controls.Add(this.Netherrack);
+            this.Controls.Add(this.ProcentName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPlayer2);
+            this.Controls.Add(this.tbPlayer1);
+            this.Name = "Leg calculator";
+            this.Text = "Leg calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPlayer1;
+        private System.Windows.Forms.TextBox tbPlayer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ProcentName;
+        private System.Windows.Forms.Label Netherrack;
+        private System.Windows.Forms.Label Distance;
+        private System.Windows.Forms.Label Procent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbPasta;
+    }
+}
+
---
+++ MEGClac/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MEGClac
+{
+    public partial class Form1 : Form
+    {
+        int player1 = 0;
+        int player2 = 0;
+        int player2Distance = 0;
+        double totalDistance = 0;
+        double procentDone = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Procent.Text = Convert.ToString(procentDone) + "%";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try // Couldn't be bothered to check if the input is a number or text, so i just added a try catch
+            {
+                player1 = Convert.ToInt32(tbPlayer1.Text); // Takes the input and makes it a variable
+                if (player2 != 0 && player2 > 0 && player1 > 0) // Starts the calculating process if both fields has an input
+                {
+                    player2Distance = 3750000 - player2; // player2 is on the NE corner, so it's finding the distance from the wb to player2's position
+                    totalDistance = player2Distance + player1;
+                    if (totalDistance > 0 && totalDistance < 3750000)  // Checks if the distance is valid
+                    { 
+                    Distance.Text = Convert.ToString(totalDistance);
+                        Netherrack.Text = Convert.ToString(totalDistance * 24);
+                        procentDone = totalDistance / 3750000 * 100; // I had some trouble calculating procent in the field below, so i just split it in 2.
+                        Procent.Text = Convert.ToString(Math.Round(procentDone,2)) + "%"; 
+                        Procent.Text = String.Format("{0:#,##0.00}", double.Parse(Procent.Text)); 
+                         Netherrack.Text = String.Format("{0:n0}", double.Parse(Procent.Text));     //I tried to split up the 1000's like 1,000,000 instead of 1000000 but
+                        Distance.Text = String.Format("{0:n0}", double.Parse(Procent.Text));        // i just couldn't get it to work for the life of me. It still outputs the number here though
+                    }
+                    else
+                    {
+                        Distance.Text = "";
+                        Netherrack.Text = ""; // Clears fields if the distance isn't valid
+                        Procent.Text = "";
+                    }
+                }
+            }
+            catch(Exception)
+            {
+            }
+        }
+
+        private void tbPlayer2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                player2 = Convert.ToInt32(tbPlayer2.Text);
+                if (player2 != 0 && player2 > 0 && player1 > 0)
+                {
+                    player2Distance = 3750000 - player2;                    // Basically the same as player1.
+                    totalDistance = player2Distance + player1;             // I duplicated the code to player2 as well so no matter what order 
+                    if (totalDistance > 0 && totalDistance < 3750000)     // you typed the coords in, it'd calculate it.
+                    {
+                        Distance.Text = Convert.ToString(totalDistance);
+                        Netherrack.Text = Convert.ToString(totalDistance * 24);
+                        procentDone = totalDistance / 3750000 * 100;
+                        Procent.Text = Convert.ToString(Math.Round(procentDone, 2)) + "%";
+                        Netherrack.Text = String.Format("{0:n0}", double.Parse(Procent.Text));
+                        Distance.Text = String.Format("{0:n0}", double.Parse(Procent.Text));
+                    }
+                    else
+                    {
+                        Distance.Text = "";
+                        Netherrack.Text = "";
+                        Procent.Text = "";
+                    }
+                }
+                }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)    // This is for the copy paste field. The textbox's name tbPasta refers to Copy Pasta
+        {                             
+            tbPasta.Text = "";
+            tbPasta.Text = "Leg 1 current progress: **" + Convert.ToString(Math.Round(procentDone, 2)) + "% done**";
+            tbPasta.Text = tbPasta.Text + "\r\n";
+            tbPasta.Text = tbPasta.Text + "\r\n";
+            tbPasta.Text = tbPasta.Text + "Total distance dug: " + Distance.Text + "/3750000";
+            tbPasta.Text = tbPasta.Text + "\r\n";
+            tbPasta.Text = tbPasta.Text + "Netherrack dug: " + Netherrack.Text + "/90000000";
+            tbPasta.Text = tbPasta.Text + "\r\n";
+            tbPasta.Text = tbPasta.Text + "\r\n";
+            tbPasta.Text = tbPasta.Text + "Digger 1 position: " + tbPlayer1.Text + ", -3750000";
+            tbPasta.Text = tbPasta.Text + "\r\n";
+            tbPasta.Text = tbPasta.Text + "Digger 2 position: " + tbPlayer2.Text + ", -3750000"; // It could probably be more efficient, but idgaf it works
+
+        }
+    }
+}
