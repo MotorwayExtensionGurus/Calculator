735d0802b86e1f0325d71f6f3ca78c5f8dbdea7e
Jonathan <70881466+Jonanen@users.noreply.github.com>
2021-02-14 03:10:38 +0100

    Add files via upload

--- MEGClac/Form1.Designer.cs
+++ MEGClac/Form1.Designer.cs
@@ -41,11 +41,28 @@ namespace MEGClac
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.tbPasta = new System.Windows.Forms.TextBox();
+            this.tbOldN = new System.Windows.Forms.TextBox();
+            this.tbOldNE = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbDays = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.daysLeft = new System.Windows.Forms.Label();
+            this.distancePerDay = new System.Windows.Forms.Label();
+            this.NetherrackPerDay = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbPlayer1
             // 
-            this.tbPlayer1.Location = new System.Drawing.Point(27, 36);
+            this.tbPlayer1.Location = new System.Drawing.Point(27, 70);
             this.tbPlayer1.Name = "tbPlayer1";
             this.tbPlayer1.Size = new System.Drawing.Size(202, 20);
             this.tbPlayer1.TabIndex = 0;
@@ -53,7 +70,7 @@ namespace MEGClac
             // 
             // tbPlayer2
             // 
-            this.tbPlayer2.Location = new System.Drawing.Point(27, 73);
+            this.tbPlayer2.Location = new System.Drawing.Point(27, 96);
             this.tbPlayer2.Name = "tbPlayer2";
             this.tbPlayer2.Size = new System.Drawing.Size(202, 20);
             this.tbPlayer2.TabIndex = 1;
@@ -82,41 +99,39 @@ namespace MEGClac
             this.ProcentName.AutoSize = true;
             this.ProcentName.Location = new System.Drawing.Point(100, 162);
             this.ProcentName.Name = "ProcentName";
-            this.ProcentName.Size = new System.Drawing.Size(72, 13);
+            this.ProcentName.Size = new System.Drawing.Size(44, 13);
             this.ProcentName.TabIndex = 4;
-            this.ProcentName.Text = "ProcentName";
+            this.ProcentName.Text = "Procent";
             // 
             // Netherrack
             // 
             this.Netherrack.AutoSize = true;
             this.Netherrack.Location = new System.Drawing.Point(24, 132);
             this.Netherrack.Name = "Netherrack";
-            this.Netherrack.Size = new System.Drawing.Size(35, 13);
+            this.Netherrack.Size = new System.Drawing.Size(0, 13);
             this.Netherrack.TabIndex = 5;
-            this.Netherrack.Text = "label3";
+            this.Netherrack.Click += new System.EventHandler(this.Netherrack_Click);
             // 
             // Distance
             // 
             this.Distance.AutoSize = true;
             this.Distance.Location = new System.Drawing.Point(181, 132);
             this.Distance.Name = "Distance";
-            this.Distance.Size = new System.Drawing.Size(35, 13);
+            this.Distance.Size = new System.Drawing.Size(0, 13);
             this.Distance.TabIndex = 6;
-            this.Distance.Text = "label4";
             // 
             // Procent
             // 
             this.Procent.AutoSize = true;
             this.Procent.Location = new System.Drawing.Point(119, 175);
             this.Procent.Name = "Procent";
-            this.Procent.Size = new System.Drawing.Size(35, 13);
+            this.Procent.Size = new System.Drawing.Size(0, 13);
             this.Procent.TabIndex = 7;
-            this.Procent.Text = "label5";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(235, 43);
+            this.label3.Location = new System.Drawing.Point(235, 77);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(15, 13);
             this.label3.TabIndex = 8;
@@ -125,7 +140,7 @@ namespace MEGClac
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(235, 80);
+            this.label4.Location = new System.Drawing.Point(235, 103);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(22, 13);
             this.label4.TabIndex = 9;
@@ -149,11 +164,177 @@ namespace MEGClac
             this.tbPasta.Size = new System.Drawing.Size(245, 170);
             this.tbPasta.TabIndex = 12;
             // 
+            // tbOldN
+            // 
+            this.tbOldN.Location = new System.Drawing.Point(339, 70);
+            this.tbOldN.Name = "tbOldN";
+            this.tbOldN.Size = new System.Drawing.Size(205, 20);
+            this.tbOldN.TabIndex = 13;
+            this.tbOldN.TextChanged += new System.EventHandler(this.tbOldN_TextChanged);
+            // 
+            // tbOldNE
+            // 
+            this.tbOldNE.Location = new System.Drawing.Point(339, 96);
+            this.tbOldNE.Name = "tbOldNE";
+            this.tbOldNE.Size = new System.Drawing.Size(205, 20);
+            this.tbOldNE.TabIndex = 14;
+            this.tbOldNE.TextChanged += new System.EventHandler(this.tbOldNE_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(336, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "ETA Calculator. This is optional";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(298, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "N";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(291, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "NE";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(376, 54);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Old coordinates";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(346, 132);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(123, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Days since these coords";
+            // 
+            // tbDays
+            // 
+            this.tbDays.Location = new System.Drawing.Point(505, 129);
+            this.tbDays.Name = "tbDays";
+            this.tbDays.Size = new System.Drawing.Size(39, 20);
+            this.tbDays.TabIndex = 20;
+            this.tbDays.TextChanged += new System.EventHandler(this.tbDays_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(336, 175);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Days left";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(460, 175);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(87, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Distance per day";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(392, 234);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(119, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Netherrack dug per day";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(343, 203);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 13);
+            this.label13.TabIndex = 24;
+            // 
+            // daysLeft
+            // 
+            this.daysLeft.AutoSize = true;
+            this.daysLeft.Location = new System.Drawing.Point(336, 203);
+            this.daysLeft.Name = "daysLeft";
+            this.daysLeft.Size = new System.Drawing.Size(41, 13);
+            this.daysLeft.TabIndex = 25;
+            this.daysLeft.Text = "label14";
+            // 
+            // distancePerDay
+            // 
+            this.distancePerDay.AutoSize = true;
+            this.distancePerDay.Location = new System.Drawing.Point(463, 204);
+            this.distancePerDay.Name = "distancePerDay";
+            this.distancePerDay.Size = new System.Drawing.Size(41, 13);
+            this.distancePerDay.TabIndex = 26;
+            this.distancePerDay.Text = "label15";
+            // 
+            // NetherrackPerDay
+            // 
+            this.NetherrackPerDay.AutoSize = true;
+            this.NetherrackPerDay.Location = new System.Drawing.Point(416, 259);
+            this.NetherrackPerDay.Name = "NetherrackPerDay";
+            this.NetherrackPerDay.Size = new System.Drawing.Size(0, 13);
+            this.NetherrackPerDay.TabIndex = 27;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(493, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Deactivate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(85, 54);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(87, 13);
+            this.label14.TabIndex = 29;
+            this.label14.Text = "New coordinates";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(275, 385);
+            this.ClientSize = new System.Drawing.Size(580, 379);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.NetherrackPerDay);
+            this.Controls.Add(this.distancePerDay);
+            this.Controls.Add(this.daysLeft);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tbDays);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbOldNE);
+            this.Controls.Add(this.tbOldN);
             this.Controls.Add(this.tbPasta);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
@@ -166,7 +347,7 @@ namespace MEGClac
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbPlayer2);
             this.Controls.Add(this.tbPlayer1);
-            this.Name = "Leg calculator";
+            this.Name = "Form1";
             this.Text = "Leg calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -187,6 +368,23 @@ namespace MEGClac
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox tbPasta;
+        private System.Windows.Forms.TextBox tbOldN;
+        private System.Windows.Forms.TextBox tbOldNE;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbDays;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label daysLeft;
+        private System.Windows.Forms.Label distancePerDay;
+        private System.Windows.Forms.Label NetherrackPerDay;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label14;
     }
 }
 
--- MEGClac/Form1.cs
+++ MEGClac/Form1.cs
@@ -12,11 +12,21 @@ namespace MEGClac
 {
     public partial class Form1 : Form
     {
+        int x = 0;
         int player1 = 0;
         int player2 = 0;
+        int oldCoordsN = 0;
+        int oldCoordsNE = 0;
         int player2Distance = 0;
+        int oldCoordsNEDistance = 0;
+        double netherrackPerDay = 0;
+        double DaysLeft = 0;
+        double oldNetherrack = 0;
+        double DistancePerDay = 0;
         double totalDistance = 0;
+        double oldtotalDistance;
         double procentDone = 0;
+        bool ETACalc = true;
         public Form1()
         {
             InitializeComponent();
@@ -39,8 +49,8 @@ namespace MEGClac
                         procentDone = totalDistance / 3750000 * 100; // I had some trouble calculating procent in the field below, so i just split it in 2.
                         Procent.Text = Convert.ToString(Math.Round(procentDone,2)) + "%"; 
                         Procent.Text = String.Format("{0:#,##0.00}", double.Parse(Procent.Text)); 
-                         Netherrack.Text = String.Format("{0:n0}", double.Parse(Procent.Text));     //I tried to split up the 1000's like 1,000,000 instead of 1000000 but
-                        Distance.Text = String.Format("{0:n0}", double.Parse(Procent.Text));        // i just couldn't get it to work for the life of me. It still outputs the number here though
+                         Netherrack.Text = String.Format("{0:n0}", double.Parse(Netherrack.Text));     //I tried to split up the 1000's like 1,000,000 instead of 1000000 but
+                        Distance.Text = String.Format("{0:n0}", double.Parse(Distance.Text));        // i just couldn't get it to work for the life of me. It still outputs the number here though
                     }
                     else
                     {
@@ -48,6 +58,12 @@ namespace MEGClac
                         Netherrack.Text = ""; // Clears fields if the distance isn't valid
                         Procent.Text = "";
                     }
+                }
+                else
+                {
+                    Distance.Text = "";
+                    Netherrack.Text = "";
+                    Procent.Text = "";
                 }
             }
             catch(Exception)
@@ -70,8 +86,8 @@ namespace MEGClac
                         Netherrack.Text = Convert.ToString(totalDistance * 24);
                         procentDone = totalDistance / 3750000 * 100;
                         Procent.Text = Convert.ToString(Math.Round(procentDone, 2)) + "%";
-                        Netherrack.Text = String.Format("{0:n0}", double.Parse(Procent.Text));
-                        Distance.Text = String.Format("{0:n0}", double.Parse(Procent.Text));
+                        Netherrack.Text = String.Format("{0:n0}", double.Parse(Netherrack.Text));
+                        Distance.Text = String.Format("{0:n0}", double.Parse(Distance.Text));
                     }
                     else
                     {
@@ -80,7 +96,13 @@ namespace MEGClac
                         Procent.Text = "";
                     }
                 }
+                else
+                {
+                    Distance.Text = "";
+                    Netherrack.Text = "";
+                    Procent.Text = "";
                 }
+            }
             catch (Exception)
             {
             }
@@ -90,17 +112,198 @@ namespace MEGClac
         {                             
             tbPasta.Text = "";
             tbPasta.Text = "Leg 1 current progress: **" + Convert.ToString(Math.Round(procentDone, 2)) + "% done**";
+            if (ETACalc == true)
+            {
+                tbPasta.Text = tbPasta.Text + "\r\n";
+                tbPasta.Text = tbPasta.Text + "ETA: **" + daysLeft.Text + " days**";
+            }
             tbPasta.Text = tbPasta.Text + "\r\n";
             tbPasta.Text = tbPasta.Text + "\r\n";
             tbPasta.Text = tbPasta.Text + "Total distance dug: " + Distance.Text + "/3750000";
             tbPasta.Text = tbPasta.Text + "\r\n";
             tbPasta.Text = tbPasta.Text + "Netherrack dug: " + Netherrack.Text + "/90000000";
+            if (ETACalc == true)
+            {
+            tbPasta.Text = tbPasta.Text + "\r\n";
+                tbPasta.Text = tbPasta.Text + "Distance per day currently: " + distancePerDay.Text;
+                tbPasta.Text = tbPasta.Text + "\r\n";
+                tbPasta.Text = tbPasta.Text + "Netherrack per day currently: " + NetherrackPerDay.Text;
+            }
             tbPasta.Text = tbPasta.Text + "\r\n";
             tbPasta.Text = tbPasta.Text + "\r\n";
             tbPasta.Text = tbPasta.Text + "Digger 1 position: " + tbPlayer1.Text + ", -3750000";
             tbPasta.Text = tbPasta.Text + "\r\n";
             tbPasta.Text = tbPasta.Text + "Digger 2 position: " + tbPlayer2.Text + ", -3750000"; // It could probably be more efficient, but idgaf it works
 
+
+        }
+
+        private void Netherrack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(ETACalc == true)
+            {
+                ETACalc = false;
+                tbOldN.Enabled = false;
+                tbOldNE.Enabled = false;
+                tbDays.Enabled = false;
+                button2.Text = "Activate";
+            }
+            else
+            {
+                ETACalc = true;
+                tbOldN.Enabled = true;
+                tbOldNE.Enabled = true;
+                tbDays.Enabled = true;
+                button2.Text = "Deactivate";
+            }
+        }
+
+        private void tbOldN_TextChanged(object sender, EventArgs e)
+        {
+                    oldCoordsN = Convert.ToInt32(tbOldN.Text); // Takes the input and makes it a variable
+
+                try // Couldn't be bothered to check if the input is a number or text, so i just added a try catch
+                {
+                if (oldCoordsNE > 0 && oldCoordsNE > 0 && oldCoordsN > 0 && player1 > 0 && player2 > 0 && Convert.ToInt32(tbDays.Text) > 0) // Starts the calculating process if all fields has an input
+                {
+                    oldCoordsNEDistance = 3750000 - oldCoordsNE; // oldCoordsNE is on the NE corner, so it's finding the distance from the wb to oldCoordsNE's position
+                    oldtotalDistance = oldCoordsNEDistance + oldCoordsN;
+                    if (oldtotalDistance > 0 && oldtotalDistance < 3750000)  // Checks if the distance is valid
+                    {
+                        // Distance per day
+                        x = Convert.ToInt32(tbDays.Text);
+                        DistancePerDay = Math.Round((totalDistance - oldtotalDistance) / x);
+                        if (DistancePerDay < 0) { DistancePerDay = DistancePerDay * -1; } // I have absolutely no idea why it sometimes returns a negative value
+                        distancePerDay.Text = Convert.ToString(DistancePerDay);
+
+                        // Netherrack per Day
+                        oldNetherrack = oldtotalDistance * 24;
+                        netherrackPerDay = (totalDistance * 24 - oldNetherrack) / Convert.ToDouble(tbDays.Text);
+                        if (netherrackPerDay < 0) { netherrackPerDay = netherrackPerDay * -1; }
+                        NetherrackPerDay.Text = Convert.ToString(netherrackPerDay);
+                        NetherrackPerDay.Text = String.Format("{0:n0}", double.Parse(NetherrackPerDay.Text));
+
+                        // Days left
+                        DaysLeft = Math.Round((3750000 - totalDistance) / DistancePerDay, 2);
+                        daysLeft.Text = Convert.ToString(DaysLeft);
+                    }
+                    else
+                    {
+                        NetherrackPerDay.Text = "";
+                        distancePerDay.Text = "";
+                        daysLeft.Text = "";
+                    }
+                }
+                else
+                {
+                    NetherrackPerDay.Text = "";
+                    distancePerDay.Text = "";
+                    daysLeft.Text = "";
+                }
+            }
+                catch (Exception)
+                {
+                }
+        }
+
+        private void tbOldNE_TextChanged(object sender, EventArgs e)
+        {
+                oldCoordsNE = Convert.ToInt32(tbOldNE.Text); // Takes the input and makes it a variable
+
+            try // Couldn't be bothered to check if the input is a number or text, so i just added a try catch
+            {
+                if (oldCoordsNE > 0 && oldCoordsNE > 0 && oldCoordsN > 0 && player1 > 0 && player2 > 0 && Convert.ToInt32(tbDays.Text) > 0) // Starts the calculating process if all fields has an input
+                {
+                    oldCoordsNEDistance = 3750000 - oldCoordsNE; // oldCoordsNE is on the NE corner, so it's finding the distance from the wb to oldCoordsNE's position
+                    oldtotalDistance = oldCoordsNEDistance + oldCoordsN;
+                    if (oldtotalDistance > 0 && oldtotalDistance < 3750000)  // Checks if the distance is valid
+                    {
+                        // Distance per day
+                        x = Convert.ToInt32(tbDays.Text);
+                        DistancePerDay = Math.Round((totalDistance - oldtotalDistance) / x);
+                        if (DistancePerDay < 0) { DistancePerDay = DistancePerDay * -1; } // I have absolutely no idea why it sometimes returns a negative value
+                        distancePerDay.Text = Convert.ToString(DistancePerDay);
+
+                        // Netherrack per Day
+                        oldNetherrack = oldtotalDistance * 24;
+                        netherrackPerDay = (totalDistance * 24 - oldNetherrack) / Convert.ToDouble(tbDays.Text);
+                        if (netherrackPerDay < 0) { netherrackPerDay = netherrackPerDay * -1; }
+                        NetherrackPerDay.Text = Convert.ToString(netherrackPerDay);
+                        NetherrackPerDay.Text = String.Format("{0:n0}", double.Parse(NetherrackPerDay.Text));
+
+                        // Days left
+                        DaysLeft = Math.Round((3750000 - totalDistance) / DistancePerDay, 2);
+                        daysLeft.Text = Convert.ToString(DaysLeft);
+                    }
+                    else
+                    {
+                        NetherrackPerDay.Text = "";
+                        distancePerDay.Text = "";
+                        daysLeft.Text = "";
+                    }
+                }
+                else
+                {
+                    NetherrackPerDay.Text = "";
+                    distancePerDay.Text = "";
+                    daysLeft.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void tbDays_TextChanged(object sender, EventArgs e)
+        {
+
+            try // Couldn't be bothered to check if the input is a number or text, so i just added a try catch
+            {
+                if (oldCoordsNE > 0 && oldCoordsNE > 0 && oldCoordsN > 0 && player1 > 0 && player2 > 0 && Convert.ToInt32(tbDays.Text) > 0) // Starts the calculating process if all fields has an input
+                {
+                    oldCoordsNEDistance = 3750000 - oldCoordsNE; // oldCoordsNE is on the NE corner, so it's finding the distance from the wb to oldCoordsNE's position
+                    oldtotalDistance = oldCoordsNEDistance + oldCoordsN;
+                    if (oldtotalDistance > 0 && oldtotalDistance < 3750000)  // Checks if the distance is valid
+                    {
+                        // Distance per day
+                        x = Convert.ToInt32(tbDays.Text);
+                        DistancePerDay = Math.Round((totalDistance - oldtotalDistance) / x);
+                        if (DistancePerDay < 0) { DistancePerDay = DistancePerDay * -1; } // I have absolutely no idea why it sometimes returns a negative value
+                        distancePerDay.Text = Convert.ToString(DistancePerDay);
+
+                        // Netherrack per Day
+                        oldNetherrack = oldtotalDistance * 24;
+                        netherrackPerDay = (totalDistance * 24 - oldNetherrack) / Convert.ToDouble(tbDays.Text);
+                            if (netherrackPerDay < 0) { netherrackPerDay = netherrackPerDay * -1; }
+                        NetherrackPerDay.Text = Convert.ToString(netherrackPerDay);
+                        NetherrackPerDay.Text = String.Format("{0:n0}", double.Parse(NetherrackPerDay.Text));
+
+                        // Days left
+                        DaysLeft = Math.Round((3750000 - totalDistance) / DistancePerDay,2);
+                        daysLeft.Text = Convert.ToString(DaysLeft);
+                    }
+                    else
+                    {
+                        NetherrackPerDay.Text = "";
+                        distancePerDay.Text = "";
+                        daysLeft.Text = "";
+                    }
+                }
+                else
+                {
+                    NetherrackPerDay.Text = "";
+                    distancePerDay.Text = "";
+                    daysLeft.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
